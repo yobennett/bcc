e868a6d4363154c0578ebd449381b8c861f1b329
John Bennett <bennett@yobennett.com>
2017-11-20 16:58:13 -0800

    ipv4 tcp summary

--- tools/tcpstates.py
+++ tools/tcpstates.py
@@ -48,6 +48,15 @@ struct ipv4_data_t {
 };
 BPF_PERF_OUTPUT(ipv4_events);
 
+struct ipv4_summary_data_t {
+    u64 saddr;
+    u64 daddr;
+    u64 ports;
+    u64 bytes_received;
+    u64 bytes_acked;
+};
+BPF_PERF_OUTPUT(ipv4_summary_events);
+
 int kprobe__tcp_set_state(struct pt_regs *ctx, struct sock *sk, int state)
 {
     // lport is either used in a filter here, or later
@@ -57,7 +66,10 @@ int kprobe__tcp_set_state(struct pt_regs *ctx, struct sock *sk, int state)
     u16 dport = sk->__sk_common.skc_dport;
 
     // get throughput stats. see tcp_get_info().
+    u64 bytes_received = 0, bytes_acked = 0;
     struct tcp_sock *tp = (struct tcp_sock *)sk;
+    bytes_received = tp->bytes_received;
+    bytes_acked = tp->bytes_acked;
 
     u16 family = sk->__sk_common.skc_family;
 
@@ -81,17 +93,20 @@ int kprobe__tcp_set_state(struct pt_regs *ctx, struct sock *sk, int state)
         }
 
         data4.state = state;
-    if (prev_state == 0) {
-        
-    } else {
-    }
-
         ipv4_events.perf_submit(ctx, &data4, sizeof(data4));
     }
 
     // reset state tracker for closed sockets
     if (state == TCP_CLOSE) {
         states.delete(&sk);
+
+        struct ipv4_summary_data_t summary4 = {};
+        summary4.saddr = sk->__sk_common.skc_rcv_saddr;
+        summary4.daddr = sk->__sk_common.skc_daddr;
+        summary4.ports = ntohs(dport) + ((0ULL + lport) << 32);
+        summary4.bytes_received = bytes_received;
+        summary4.bytes_acked = bytes_acked;
+        ipv4_summary_events.perf_submit(ctx, &summary4, sizeof(summary4));
     }
 
     return 0;
@@ -111,6 +126,14 @@ class Data_ipv4(ct.Structure):
         ("state", ct.c_ulonglong)
     ]
 
+class Data_ipv4_summary(ct.Structure):
+    _fields_ = [
+        ("saddr", ct.c_ulonglong),
+        ("daddr", ct.c_ulonglong),
+        ("ports", ct.c_ulonglong),
+        ("bytes_received", ct.c_ulonglong),
+        ("bytes_acked", ct.c_ulonglong)
+    ]
 
 def tcp_state(state):
     return {
@@ -142,6 +165,13 @@ def print_ipv4_event(cpu, data, size):
         inet_ntop(AF_INET, pack("I", event.daddr)), event.ports & 0xffffffff,
         tcp_state(event.prev_state), tcp_state(event.state)))
 
+def print_ipv4_summary_event(cpu, data, size):
+    event = ct.cast(data, ct.POINTER(Data_ipv4_summary)).contents
+    print("\n{}:{} {}:{} {}b {}b\n".format(
+        inet_ntop(AF_INET, pack("I", event.saddr)), event.ports >> 32,
+        inet_ntop(AF_INET, pack("I", event.daddr)), event.ports & 0xffffffff,
+        event.bytes_received, event.bytes_acked))
+
 # initialize BPF
 b = BPF(text=bpf_text)
 
@@ -150,5 +180,6 @@ print(header_string % ("LADDR", "LPORT", "RADDR", "RPORT", "PSTATE", "STATE"))
 
 # read events
 b["ipv4_events"].open_perf_buffer(print_ipv4_event, page_cnt=64)
+b["ipv4_summary_events"].open_perf_buffer(print_ipv4_summary_event, page_cnt=64)
 while 1:
     b.kprobe_poll()
